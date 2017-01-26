bab65fcc05bb25a456bfacda30c3112029913aea
Marta Ludovico <marta.ludovico@thetrainline.com>
2017-01-26 18:21:15 +0000

    using ids for partner and children

--- hacker-games-trainline/src/hacker-games-trainline/Model/Person.cs
+++ hacker-games-trainline/src/hacker-games-trainline/Model/Person.cs
@@ -11,7 +11,7 @@ namespace hacker_games_trainline.Model
         public DateTime Birthday { get; set; }
         public string City { get; set; }
         public string Job { get; set; }
-        public Person Partner { get; set; }
-        public List<Person> Children { get; set; }
+        public int PartnerId { get; set; }
+        public List<int> ChildrenIds { get; set; }
     }
 }
