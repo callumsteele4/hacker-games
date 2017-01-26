e802ee56a4daf5b5001d2380f34d5b955a237e8a
Callum Steele <callum.steele@thetrainline.com>
2017-01-26 13:39:37 +0000

    Add SQlite DB to store Persons

---
+++ hacker-games-trainline/src/hacker-games-trainline/Migrations/20170126124543_Initial-Person.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using hacker_games_trainline.Data;
+
+namespace hackergamestrainline.Migrations
+{
+    [DbContext(typeof(PersonContext))]
+    [Migration("20170126124543_Initial-Person")]
+    partial class InitialPerson
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("hacker_games_trainline.Model.Person", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Birthday");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Persons");
+                });
+        }
+    }
+}
