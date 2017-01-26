9d6d6774814761f23d108dde0a04d2a1b22e9b94
Natalie Akam <Natalie.Akam@thetrainline.com>
2017-01-26 14:00:39 +0000

    Adds Redux and React Router

--- hacker-games-trainline/src/hacker-games-trainline/hacker-games-web/src/index.js
+++ hacker-games-trainline/src/hacker-games-trainline/hacker-games-web/src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Map } from 'immutable';
+import thunk from 'redux-thunk';
+import * as createLogger from 'redux-logger';
+import rootReducer from './reducers';
+import './styles/main.css';
+
+const env = process.env.NODE_ENV;
+const middlewares = [ thunk ];
+
+if (env === 'dev') {
+  middlewares.push(createLogger());
+}
+
+const store = createStore(rootReducer, Map(), applyMiddleware(...middlewares));
+const history = syncHistoryWithStore(browserHistory, store, {
+    selectLocationState: (state) => state.get('routing')
+  });
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
