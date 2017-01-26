68686c93be246e3b2ae15394187199d1db0ce296
shakmb <shakm@hotmail.com>
2017-01-26 19:37:59 +0000

    Added more data for users

--- hacker-games-trainline/src/hacker-games-trainline/Startup.cs
+++ hacker-games-trainline/src/hacker-games-trainline/Startup.cs
@@ -55,8 +55,8 @@ namespace hacker_games_trainline
             CurrentUser.User = new Person {Name = "Natalie Akam", ChildrenIds = new List<int> {callumId}, PartnerId = tomId};
             Persons.AddPerson(CurrentUser.User);
 
-            var callum = new Person { Name = "Callum Steele" };
-            var marta = new Person { Name = "Marta Ludovico" };
+            var callum = new Person { Name = "Callum Steele" , City = "London", Job = "Developer", Note = "Callum likes to party!"};
+            var marta = new Person { Name = "Marta Ludovico" , City = "Reading", Job = "Developer", Note = "Marta hurt her ankle"};
             var alexandre = new Person { Name = "Alexandre Rieux", ChildrenIds = new List<int> {natalieId, martaId}, PartnerId = shakeelId};
             var shakeel = new Person { Name = "Shakeel Mohammed", ChildrenIds = new List<int> {natalieId, martaId}, PartnerId = alexandreId};
             var tom = new Person { Name = "Tom Price", ChildrenIds = new List<int> {callumId}, PartnerId = natalieId};
