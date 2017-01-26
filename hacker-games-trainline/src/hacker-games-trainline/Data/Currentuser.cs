9fad0cf45e87a0036072ee3be36c754523528afe
Callum Steele <callum.steele@thetrainline.com>
2017-01-26 16:55:12 +0000

    Add basic user at startup

---
+++ hacker-games-trainline/src/hacker-games-trainline/Data/Currentuser.cs
@@ -0,0 +1,9 @@
+﻿using hacker_games_trainline.Model;
+
+namespace hacker_games_trainline.Data
+{
+    public static class CurrentUser
+    {
+        public static Person User { get; set; } = new Person { Name = "Marta Ludovico" };
+    }
+}
