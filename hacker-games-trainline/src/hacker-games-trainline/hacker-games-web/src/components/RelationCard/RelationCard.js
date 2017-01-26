f4b173a04150d0bbfb7b6646a66780dbe13b9758
Alexandre Rieux <alexr.3165@gmail.com>
2017-01-26 20:15:29 +0000

    Improve card

--- hacker-games-trainline/src/hacker-games-trainline/hacker-games-web/src/components/RelationCard/RelationCard.js
+++ hacker-games-trainline/src/hacker-games-trainline/hacker-games-web/src/components/RelationCard/RelationCard.js
@@ -5,21 +5,32 @@ import sample from '../../images/sample.png';
 const styles = StyleSheet.create({
   container: {
     width: 250,
-    margin: '20px auto'
+    margin: '20px auto',
+    boxShadow: '0px 1px 8px 0px #d8d8d8'
   },
   name: {
-    margin: 0
+    margin: 0,
+    fontSize: 24,
+    color: '#6c6c6c'
   },
   imageContainer: {
     height: 200,
     overflow: 'hidden'
   },
   image: {
-    width: '100%'
+    width: '100%',
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5
   },
   description: {
     backgroundColor: '#F5F4F4',
     padding: 20,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5
+  },
+  relation: {
+    color: '#a09d9d',
+    fontSize: 14
   }
 });
 
@@ -30,7 +41,7 @@ const RelationCard = ({ relation }) => (
     </div>
     <div className={css(styles.description)}>
       <h2 className={css(styles.name)}>{ relation.name }</h2>
-      <div>Relation: your {relation.relation}</div>
+      <div className={css(styles.relation)}>Relation: your {relation.relation}</div>
     </div>
   </div>
 );
