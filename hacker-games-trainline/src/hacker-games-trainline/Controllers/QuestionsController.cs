daee0e16fd2bb3ed7019849a58aaca8a325ae8f2
Callum Steele <callum.steele@thetrainline.com>
2017-01-26 20:05:55 +0000

    Add basic questionscontroller

---
+++ hacker-games-trainline/src/hacker-games-trainline/Controllers/QuestionsController.cs
@@ -0,0 +1,22 @@
+﻿using hacker_games_trainline.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hacker_games_trainline.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuestionsController : Controller
+    {
+        private readonly QuestionGenerator _questionGenerator;
+
+        public QuestionsController()
+        {
+            _questionGenerator = new QuestionGenerator();
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(_questionGenerator.RandomQuestion());
+        }
+    }
+}
