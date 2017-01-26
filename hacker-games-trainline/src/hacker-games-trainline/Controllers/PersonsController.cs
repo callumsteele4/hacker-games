1f1d8eedd9d7bda956859bccc3c2ecfa653d1dd7
Callum Steele <callum.steele@thetrainline.com>
2017-01-26 16:16:32 +0000

    Add graph database structure
    In-memory graph to represent family.

--- hacker-games-trainline/src/hacker-games-trainline/Controllers/PersonsController.cs
+++ hacker-games-trainline/src/hacker-games-trainline/Controllers/PersonsController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+<<<<<<< Updated upstream
 using System.Linq;
 using System.Threading.Tasks;
+=======
+using hacker_games_trainline.Data;
+>>>>>>> Stashed changes
 using hacker_games_trainline.Model;
 using Microsoft.AspNetCore.Mvc;
 using hacker_games_trainline.Data;
@@ -20,6 +24,7 @@ namespace hacker_games_trainline.Controllers
         [HttpGet]
         public JsonResult Get()
         {
+<<<<<<< Updated upstream
             return Json(
                 _personsContext.Persons.ToList());
         }
@@ -37,6 +42,33 @@ namespace hacker_games_trainline.Controllers
             await _personsContext.AddAsync(person);
             _personsContext.SaveChanges();
             return Json(person);
+=======
+            return Json(Persons.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            return Json(Persons.FindPerson(id));
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Person person)
+        {
+            Persons.AddPerson(person, null);
+
+            return Json(person);
+        }
+    }
+
+    [Route("api/[controller]")]
+    public class RelationsShipsController : Controller
+    {
+        [HttpPost]
+        public void Relationship(int fromId, int toId, string relationshipTag)
+        {
+            Persons.AddRelationship(fromId, toId, relationshipTag);
+>>>>>>> Stashed changes
         }
     }
 }
