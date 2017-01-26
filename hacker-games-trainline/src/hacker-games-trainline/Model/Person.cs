b2472efe0023992291ad37b4e2942819d29332c6
Callum Steele <callum.steele@thetrainline.com>
2017-01-26 12:07:40 +0000

    Setup basic api
    Returning basic Person object with hard-coded values.

---
+++ hacker-games-trainline/src/hacker-games-trainline/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace hacker_games_trainline.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public Photo Photo { get; set; }
+    }
+}
